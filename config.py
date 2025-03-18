32020f649bcce647bca24eeedbba4c5e526df433
hskhyl <kannakawai9@gmail.com>
2025-03-18 17:33:57 +0900

    config 관련(엔드포인트, api_key 입력란

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+API_KEY = ""
+
+# 길드 관련 API 엔드포인트
+GUILD_ID_URL = "https://open.api.nexon.com/maplestory/v1/guild/id"
+GUILD_BASIC_URL = "https://open.api.nexon.com/maplestory/v1/guild/basic"
+
+# 캐릭터 관련 API 엔드포인트
+CHARACTER_ID_URL = "https://open.api.nexon.com/maplestory/v1/id"
+CHARACTER_BASIC_URL = "https://open.api.nexon.com/maplestory/v1/character/basic"
