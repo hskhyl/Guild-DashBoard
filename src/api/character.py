dc0b30bb5fb85c3818e4321359a622fef64428d7
hskhyl <kannakawai9@gmail.com>
2025-03-19 15:15:57 +0900

    character 관련 API 호출 함수 수정(스공, 캐릭터생성일 추가)

--- src/api/character.py
+++ src/api/character.py
@@ -1,31 +1,50 @@
-# src/api/character.py
-
 import requests
 
-from config import API_KEY, CHARACTER_BASIC_URL, CHARACTER_ID_URL
+from config import API_KEY, CHARACTER_BASIC_URL, CHARACTER_FINAL_STAT_URL, CHARACTER_ID_URL, CHARACTER_UNION_INFO_URL
 from src.utils import global_rate_limit
+
+
+def fetch_api(url, params):
+    """공통 API 요청 함수"""
+    headers = {"accept": "application/json", "x-nxopen-api-key": API_KEY}
+    response = requests.get(url, headers=headers, params=params)
+    # 강제로 UTF-8 인코딩 설정
+    response.encoding = "utf-8"
+    if response.status_code == 200:
+        try:
+            return response.json()
+        except ValueError:
+            raise Exception(f"API 응답이 JSON 형식이 아님: {url} -> {response.text}")
+    else:
+        raise Exception(f"API 호출 실패 ({url}): {response.status_code} {response.text}")
 
 
 @global_rate_limit
 def get_character_ocid(character_name):
     """캐릭터 이름을 받아 ocid 조회"""
-    headers = {"accept": "application/json", "x-nxopen-api-key": API_KEY}
     params = {"character_name": character_name}
-    response = requests.get(CHARACTER_ID_URL, headers=headers, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        return data.get("ocid")
-    else:
-        raise Exception(f"Character ID API 호출 실패 ({character_name}): {response.status_code} {response.text}")
+    data = fetch_api(CHARACTER_ID_URL, params)
+    return data.get("ocid")
 
 
 @global_rate_limit
 def get_character_basic(ocid, date):
-    """ocid와 조회 날짜(어제 날짜)를 받아 캐릭터 기본 정보 조회"""
-    headers = {"accept": "application/json", "x-nxopen-api-key": API_KEY}
+    """ocid와 조회 날짜를 받아 캐릭터 기본 정보 조회"""
     params = {"ocid": ocid, "date": date}
-    response = requests.get(CHARACTER_BASIC_URL, headers=headers, params=params)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise Exception(f"Character Basic API 호출 실패 (ocid: {ocid}): {response.status_code} {response.text}")
+    return fetch_api(CHARACTER_BASIC_URL, params)
+
+
+@global_rate_limit
+def get_character_stat(ocid, date):
+    """캐릭터 최소 스탯 공격력 조회"""
+    params = {"ocid": ocid, "date": date}
+    data = fetch_api(CHARACTER_FINAL_STAT_URL, params)
+    return float(data["final_stat"][0]["stat_value"])
+
+
+@global_rate_limit
+def get_union_info(ocid, date):
+    """유니온 정보 조회"""
+    params = {"ocid": ocid, "date": date}
+    data = fetch_api(CHARACTER_UNION_INFO_URL, params)
+    return data.get("union_level")
